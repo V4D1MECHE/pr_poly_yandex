73e7a5272e610ff67186780a24bd7f3823465f01
root <root@zen>
2024-01-17 11:38:04 +0300

    стиль -  имеется, js - нет. На 4

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 
-const mainUrl = "http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/"
+const mainUrl = "http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes"
+apiKey = "942eb11c-0388-4a80-b009-70c62b6d471f"
 
-function getOrders(){
-    const url = new URL ("orders", mainUrl);
+function getRoutes(){
+    const url = new URL ("routes", mainUrl);
     url.searchParams.set("api_key", '942eb11c-0388-4a80-b009-70c62b6d471f');
 
-    let xhr = new XMLHttpRequest();
-    xhr.open('get', url);
-    xhr.onload = function() {
-        console.log(this.response)
-    }
-    xhr.send();
+    fetch(url).then(response => { 
+        return response.json();
+    }).then(response => { 
+        console.log(response) 
+    })
+
 }
 
 
-window.addEventListener('DOMContentLoaded', getOrders)
+function hello(){
+    const url = mainUrl;
 
+    fetch(url).then(response => { 
+        return response.json();
+    }).then(response => { 
+        console.log(response) 
+    })
+}
+
+window.addEventListener('DOMContentLoaded', getRoutes)
